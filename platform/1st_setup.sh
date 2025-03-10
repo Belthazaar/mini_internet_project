6ad7141129266ff44e3a1a33e5563a892ce0f2c4
Christoff Visser <christoffvis@gmail.com>
2025-03-10 13:15:31 +0000

    Clean up 1st start script and add docker pull relevant images

--- platform/1st_setup.sh
+++ platform/1st_setup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if (($UID != 0)); then
+    echo "$0 needs to be run as root"
+    exit 1
+fi
 
-sudo apt update && sudo apt install openvpn openvswitch-switch openvpn linux-modules-extra-`uname -r` -y
-sudo sysctl fs.inotify.max_user_instances=1024
+apt update
+apt install openvpn openvswitch-switch openvpn linux-modules-extra-`uname -r` -y
+sysctl fs.inotify.max_user_instances=1024
+
+bash ./docker_images/pull_images.sh
